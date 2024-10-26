0d8c95eee8ddce925ab6b25997d71f6caec28de1
hermescolina <itloboc@gmail.com>
2024-10-26 19:14:50 +0800

    Switch to using tfe_outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 ##################################################################################
 # OUTPUT
 ##################################################################################
+
+output "vpc_id" {
+  value       = module.main.vpc_id
+  description = "VPC_ID"
+}
+
+output "public" {
+  value       = module.main.public_subnets
+  description = "List of Public Subnets"
+}
